8c65abd0acd174d472afc9d3a90620f89b3528db
Larry Kollar <lkollar@gmail.com>
2022-11-28 14:53:02 -0500

    Cleaned up fixext

--- fixext.sh
+++ fixext.sh
@@ -5,11 +5,10 @@
 # usage: cd [bookdirectory]; sh [me]
 
 for i in *.md; do
-# f=`basename $i ".md"`
-  f=`echo $i | sed -e 's/hacloudapiv1ecs//' -e 's/\.md$/.dita/'`
+  f=`echo $i | sed -e 's/\.md$/.dita/'`
   mv $i ${f}
 done
 
 for i in *.dita *.ditamap; do
-  sed -i -e 's/_hacloudapiv1ecs/_/g' -e 's/href="\([^.]*\).md"/href="\1.dita"/g' $i
+  sed -i -e 's/href="\([^.]*\).md"/href="\1.dita"/g' $i
 done
